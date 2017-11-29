1b55e0e3c4014bb3ef9252880eaf33ea72eef2a0
Ayush <Ayush-15@live.com>
2017-11-29 14:09:47 +0530

    Intialised

---
+++ index.js
@@ -0,0 +1,38 @@
+
+var restify = require('restify');
+var builder = require('botbuilder');
+var cognitiveservices = require('../../lib/botbuilder-cognitiveservices');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+ appId: process.env.MICROSOFT_APP_ID,
+ appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var recognizer = new cognitiveservices.QnAMakerRecognizer({
+	knowledgeBaseId: '6b36aedd-5f70-4dbd-80d4-01a4cb8e6b55', 
+	subscriptionKey: '7c096200dabd43e7b9c9d4587b172f6f'});
+	
+var basicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
+	recognizers: [recognizer],
+	defaultMessage: 'No match! Try changing the query terms!',
+	qnaThreshold: 0.3
+});
+
+bot.dialog('/', basicQnAMakerDialog);
